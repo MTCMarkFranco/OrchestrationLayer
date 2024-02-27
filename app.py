cbef4470bc0c4daf91aa4afd6e0053a3e0698550
Mark Franco <marfra@microsoft.com>
2024-02-27 18:33:56 -0500

    Looking good so far

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # Package Imports
-from flask import Flask, request
+import websockets
+from geventwebsocket.handler import WebSocketHandler
 from dotenv import dotenv_values
 import asyncio
 import sys
@@ -11,9 +12,6 @@ from models.data_models import Action
 from services.kernel_service import kernel_proxy
 from services.logger_service import logger_proxy
 
-# Initialize the webserver  
-app = Flask(__name__, template_folder="templates", static_folder="static")
-
 if platform.system() == "Windows" and sys.version_info >= (3, 8, 0):
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     
@@ -24,20 +22,16 @@ logger_svc= logger_proxy.get_logger_service()
 # Initialize configuration via .env file
 config = dotenv_values(".env")
 
-
 async def generateSynthesis(records):
-
     global kernel_svc
     global logger_svc
 
-    # Get the response from the last step in the plan
-    chatTurnAPIResponse = (await kernel_svc.semantic_plugins["generate_synthesis"].invoke(input=json.dumps(records))).result
-                               
-    # Get the response from the action above and prepare for return...
-    logger_svc.logger.debug(chatTurnAPIResponse)
-    logger_svc.logger.info("Chat Turn Complete! Returning Synthesis...")
-      
-    return chatTurnAPIResponse
+    # Get the response stream from the last step in the plan
+    response_stream = kernel_svc.semantic_plugins["generate_synthesis"].invoke_stream(input=json.dumps(records))
+
+    async for response in response_stream:
+        # Process each response here
+        yield response.result
 
 async def processQuery(query):
        
@@ -54,31 +48,27 @@ async def processQuery(query):
       
     return chatTurnAPIResponse
  
-@app.route("/")
-def root():
-    return "Hello. chat API here to help you!"
 
-@app.route('/chat', methods=['OPTIONS'])
-def options():
-    return {'Allow' : 'GET, POST, OPTIONS'}, 200, \
-    { 'Access-Control-Allow-Origin': '*', \
-      'Access-Control-Allow-Methods' : 'POST, OPTIONS', \
-      'Access-Control-Allow-Headers' : 'Content-Type' }
-       
-@app.route("/chat", methods=["POST"])
-async def chat():
+async def chat(websocket, path):
+    async for message in websocket:
+        message = json.loads(message)
         
-    body = request.get_json()
-    records = body['records']
-    
-    if(records is not None and body['generate_synthesis'] == True):
-        output = await generateSynthesis(records)
-        
-    else:
-        query = body['messages'][0]['text']
-        output = await processQuery(query)
-        
-    if output is None:
-        return {"error": "Error Getting results from Index" }, 400, {'Access-Control-Allow-Origin': '*'}
-    else:
-        return output, 200, {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'}
+        if(message is not None and json['generate_synthesis'] == True):
+            yieldedResult = await generateSynthesis(message)
+            for result in yieldedResult:
+                await websocket.send(result)
+            
+        else:
+            query = message['messages'][0]['text']
+            output = await processQuery(query)
+            if output is None:
+                await websocket.send('error', {'error': 'Error Getting results from Index'})
+            else:
+                await websocket.send(output)
+
+# Initialize the webserver
+logger_svc.logger.info("Starting webSockets on port 5000...")  
+start_server = websockets.serve(chat, "localhost", 5000)
+logger_svc.logger.info("Starting webSockets on port 5000...done! Listening for connections...")    
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
